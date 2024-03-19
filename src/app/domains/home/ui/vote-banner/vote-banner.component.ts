37a3853fb68e5294f66f622815e376443ef64c70
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-19 20:35:42 +0100

    Create `vote-banner` comoponent

---
+++ src/app/domains/home/ui/vote-banner/vote-banner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vote-banner',
+  standalone: true,
+  imports: [],
+  templateUrl: './vote-banner.component.html',
+  styleUrl: './vote-banner.component.scss'
+})
+export class VoteBannerComponent {
+
+}
