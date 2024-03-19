2f08997691db250abe24334ad8cab309bf133ac8
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-19 18:06:34 +0100

    Import `goal-section` into `home`

--- src/app/domains/home/home.component.ts
+++ src/app/domains/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { GoalSectionComponent } from './ui/goal-section/goal-section.component';
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [],
+  imports: [GoalSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
