a01e06dbb3eb1e046204f180f581eb849d889a5f
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-19 20:54:26 +0100

    Import `vote-banner` into `home`

--- src/app/domains/home/home.component.ts
+++ src/app/domains/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { GoalSectionComponent } from './ui/goal-section/goal-section.component';
+import { VoteBannerComponent } from './ui/vote-banner/vote-banner.component';
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [GoalSectionComponent],
+  imports: [GoalSectionComponent, VoteBannerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
