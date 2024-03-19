a6986037583a1bacbf64d0fa0e69075d77057fce
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-19 18:24:21 +0100

    Set up `router-outlet`

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        title: 'NASS-EU | Home',
+        loadComponent: () => import('./domains/home/home.component').then(c => c.HomeComponent)
+    }
+];
