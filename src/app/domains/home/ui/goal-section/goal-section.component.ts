7e40305429b15e88071956cff532d8027521d265
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-19 17:48:10 +0100

    Create `goal-section` component

---
+++ src/app/domains/home/ui/goal-section/goal-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'goal-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './goal-section.component.html',
+  styleUrl: './goal-section.component.scss'
+})
+export class GoalSectionComponent {
+
+}
