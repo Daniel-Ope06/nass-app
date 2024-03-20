2737c638fcca38f4626eefc4e1fc8fdbe7c2bd9e
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-20 19:23:09 +0100

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: '',
-        title: 'NASS-EU | Home',
+        title: 'Home | NASS-EU',
         loadComponent: () => import('./domains/home/home.component').then(c => c.HomeComponent)
     }
 ];
