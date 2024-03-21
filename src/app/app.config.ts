b75b7e0ef39593534aeb01fecf9a581e0f227701
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-21 13:30:34 +0100

    Add `angular / fire`

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"nass-eu","appId":"1:129803068434:web:a7ab0444e3643ea50364cb","storageBucket":"nass-eu.appspot.com","apiKey":"AIzaSyC17YWVDKGOwFebUrB7AY81nEKeX-bbRwc","authDomain":"nass-eu.firebaseapp.com","messagingSenderId":"129803068434"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
