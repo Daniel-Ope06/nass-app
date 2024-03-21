bdb8efef158751525f53a5c2c96f7052a9488ca3
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-21 18:02:09 +0100

    Create `suggestion-form` component

---
+++ src/app/domains/home/feature/suggestion-form/suggestion-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'suggestion-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './suggestion-form.component.html',
+  styleUrl: './suggestion-form.component.scss'
+})
+export class SuggestionFormComponent {
+
+}
