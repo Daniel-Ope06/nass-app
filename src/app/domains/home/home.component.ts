36a0fe3ff1205ce88ba88452621cdc80756ad15f
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-21 18:10:40 +0100

    Import `suggestion-form` into `home`

--- src/app/domains/home/home.component.ts
+++ src/app/domains/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { GoalSectionComponent } from './ui/goal-section/goal-section.component';
 import { RegistrationFormComponent } from './feature/registration-form/registration-form.component';
+import { SuggestionFormComponent } from './feature/suggestion-form/suggestion-form.component';
 import { VoteBannerComponent } from './ui/vote-banner/vote-banner.component';
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [GoalSectionComponent, RegistrationFormComponent, VoteBannerComponent],
+  imports: [GoalSectionComponent, RegistrationFormComponent, SuggestionFormComponent, VoteBannerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
