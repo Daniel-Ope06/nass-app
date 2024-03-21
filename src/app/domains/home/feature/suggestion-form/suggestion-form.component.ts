49069253c6335ca0dc98964ce7b1730ff4c9ec1d
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-21 19:55:28 +0100

    Set up `suggestion-form`

--- src/app/domains/home/feature/suggestion-form/suggestion-form.component.ts
+++ src/app/domains/home/feature/suggestion-form/suggestion-form.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Suggestion } from '../../data-access/suggestion.model';
+import { SuggestionService } from '../../data-access/suggestion.service';
 
 @Component({
   selector: 'suggestion-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './suggestion-form.component.html',
   styleUrl: './suggestion-form.component.scss'
 })
 export class SuggestionFormComponent {
+  @ViewChild('sugForm') form!: ElementRef<HTMLFormElement>;
 
+  private suggestionService: SuggestionService = inject(SuggestionService);
+
+  suggestion: Suggestion = {
+    title: '',
+    email: '',
+    message: ''
+  }
+
+  showEmailError : boolean= false;
+  showConfirmMsg: boolean = false;
+
+  canSubmit(): boolean {
+    return !(
+      this.suggestion.title.trim() === '' ||
+      this.suggestion.email.trim() === '' ||
+      this.suggestion.message.trim() === ''
+    );
+  }
+
+  async submit() {
+    if (this.isEmailValid()) {
+      this.showEmailError = false; // remove error message
+      await this.suggestionService.submitSuggestion(this.suggestion); // submit suggestion
+
+      // reset form
+      this.form.nativeElement.reset();
+      this.suggestion = { title: '', email: '', message: '' };
+
+      // show confirmation message for 5 seconds
+      this.showConfirmMsg = true;
+      setTimeout(() => this.showConfirmMsg = false, 5000);
+    } else {
+      this.showEmailError = true; // show error message
+    }
+  }
+
+  isEmailValid(): boolean {
+    const emailRegex = /^[\w\.-]+@[\w\.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(this.suggestion.email);
+  }
 }
