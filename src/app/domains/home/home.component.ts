ba234bd57d64feb26754dfdb703ea8999431991f
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-22 11:13:53 +0100

    Import `forms-slider` into `home`

--- src/app/domains/home/home.component.ts
+++ src/app/domains/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+
+// Import home domain components
+import { FormsSliderComponent } from './ui/forms-slider/forms-slider.component';
 import { GoalSectionComponent } from './ui/goal-section/goal-section.component';
 import { RegistrationFormComponent } from './feature/registration-form/registration-form.component';
 import { SuggestionFormComponent } from './feature/suggestion-form/suggestion-form.component';
@@ -7,7 +10,13 @@ import { VoteBannerComponent } from './ui/vote-banner/vote-banner.component';
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [GoalSectionComponent, RegistrationFormComponent, SuggestionFormComponent, VoteBannerComponent],
+  imports: [
+    FormsSliderComponent,
+    GoalSectionComponent,
+    RegistrationFormComponent,
+    SuggestionFormComponent,
+    VoteBannerComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
