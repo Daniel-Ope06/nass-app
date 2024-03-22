f15a21a33c87b9da1be1ea57c90d95f68a78a99a
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-22 16:42:34 +0100

    Create `events-slider` component

---
+++ src/app/domains/home/ui/events-slider/events-slider.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-slider',
+  standalone: true,
+  imports: [],
+  templateUrl: './events-slider.component.html',
+  styleUrl: './events-slider.component.scss'
+})
+export class EventsSliderComponent {
+
+}
