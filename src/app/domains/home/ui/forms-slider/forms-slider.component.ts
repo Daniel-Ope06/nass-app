8812277de1c8e365142016a0382ab0b243cb7693
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-22 16:35:47 +0100

    Set up `forms-slider`

--- src/app/domains/home/ui/forms-slider/forms-slider.component.ts
+++ src/app/domains/home/ui/forms-slider/forms-slider.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Slide } from '../../../shared/data-access/slide.model';
+import { SliderComponent } from '../../../shared/feature/slider/slider.component';
+import { NassFormService } from '../../data-access/forms/nass-form.service';
 
 @Component({
   selector: 'forms-slider',
   standalone: true,
-  imports: [],
+  imports: [SliderComponent],
   templateUrl: './forms-slider.component.html',
   styleUrl: './forms-slider.component.scss'
 })
 export class FormsSliderComponent {
+  private nassFormService: NassFormService = inject(NassFormService);
 
+  forms: Slide[] = [];
+
+  ngOnInit(): void {
+    this.nassFormService.getAllForms().then((forms) => {
+      this.forms = forms;
+    });
+  }
 }
