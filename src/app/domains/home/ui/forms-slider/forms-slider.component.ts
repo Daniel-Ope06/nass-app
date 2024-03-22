e7b19b2f65911bccd86322953a91a5b35e2c3696
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-22 11:13:28 +0100

    Create `forms-slider` component

---
+++ src/app/domains/home/ui/forms-slider/forms-slider.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'forms-slider',
+  standalone: true,
+  imports: [],
+  templateUrl: './forms-slider.component.html',
+  styleUrl: './forms-slider.component.scss'
+})
+export class FormsSliderComponent {
+
+}
