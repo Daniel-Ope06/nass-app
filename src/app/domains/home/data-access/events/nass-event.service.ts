163ff58dc33323bd95d1ae8e635a82f5222d96f5
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-22 17:07:36 +0100

    Create nass-event.service.ts

---
+++ src/app/domains/home/data-access/events/nass-event.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, inject } from '@angular/core';
+import { Firestore, collection, getDocs } from '@angular/fire/firestore';
+import { Slide } from '../../../shared/data-access/slide.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NassEventService {
+  private firestore: Firestore = inject(Firestore);
+
+  async getAllEvents(): Promise<Slide[]> {
+    let events: Slide[] = [];
+    const querySnapshot = await getDocs(collection(this.firestore, 'events'));
+    querySnapshot.forEach((doc) => {
+      events.push(doc.data() as Slide);
+    })
+    return events;
+  }
+}
