4cf117b81d4b41a1b94718e908ce5017080c9d9e
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-22 17:07:53 +0100

    Set up `events-slider`

--- src/app/domains/home/ui/events-slider/events-slider.component.ts
+++ src/app/domains/home/ui/events-slider/events-slider.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Slide } from '../../../shared/data-access/slide.model';
+import { SliderComponent } from '../../../shared/feature/slider/slider.component';
+import { NassEventService } from '../../data-access/events/nass-event.service';
 
 @Component({
   selector: 'events-slider',
   standalone: true,
-  imports: [],
+  imports: [SliderComponent],
   templateUrl: './events-slider.component.html',
   styleUrl: './events-slider.component.scss'
 })
 export class EventsSliderComponent {
+  private nassEventService: NassEventService = inject(NassEventService);
 
+  events: Slide[] = [];
+
+  ngOnInit(): void {
+    this.nassEventService.getAllEvents().then((events) => {
+      this.events = events;
+    });
+  }
 }
