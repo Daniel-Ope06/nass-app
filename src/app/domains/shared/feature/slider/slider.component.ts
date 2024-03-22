6906e81db35d68bcd5519a42ac15ce55a9d4a436
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-03-22 16:36:15 +0100

    Set up `slider` feature

--- src/app/domains/shared/feature/slider/slider.component.ts
+++ src/app/domains/shared/feature/slider/slider.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Slide } from '../../data-access/slide.model';
 
 @Component({
   selector: 'slider',
@@ -8,5 +9,37 @@ import { Component } from '@angular/core';
   styleUrl: './slider.component.scss'
 })
 export class SliderComponent {
+  @Input({required: true}) slides!: Slide[];
 
+  @ViewChild('sliderRef', { static: true }) slider!: ElementRef;
+
+  isLeftScrollDisabled: boolean = true;
+  isRightScrollDisabled: boolean = false;
+
+  openForm(url: string): void {
+    window.open(url, '_blank');
+  }
+
+  scrollLeft(): void {
+    this.slider.nativeElement.scrollLeft -= window.innerWidth/2;
+  }
+
+  scrollRight(): void {
+    this.slider.nativeElement.scrollLeft += window.innerWidth/2;
+  }
+
+  disableLeftScroll(): boolean {
+    return this.slider.nativeElement.scrollLeft === 0;
+  }
+
+  disableRightScroll(): boolean {
+    let maxScrollLeft = this.slider.nativeElement.scrollWidth - this.slider.nativeElement.clientWidth;
+    let scrollRemaining = maxScrollLeft - this.slider.nativeElement.scrollLeft;
+    return scrollRemaining <= window.innerWidth/6;
+  }
+
+  onScroll(): void {
+    this.isLeftScrollDisabled = this.disableLeftScroll();
+    this.isRightScrollDisabled = this.disableRightScroll();
+  }
 }
