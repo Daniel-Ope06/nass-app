5eeb6495ab016a0a7c84195001cd671b0b9bf403
Daniel Ope <opeoluwaoyedeji06@gmail.com>
2024-03-24 15:10:52 +0100

    Import `navbar` into `home`

--- src/app/domains/home/home.component.ts
+++ src/app/domains/home/home.component.ts
@@ -5,6 +5,7 @@ import { EventsSliderComponent } from './ui/events-slider/events-slider.componen
 import { FormsSliderComponent } from './ui/forms-slider/forms-slider.component';
 import { GoalSectionComponent } from './ui/goal-section/goal-section.component';
 import { HeroCarouselComponent } from './ui/hero-carousel/hero-carousel.component';
+import { NavbarComponent } from '../shared/ui/navbar/navbar.component';
 import { RegistrationFormComponent } from './feature/registration-form/registration-form.component';
 import { SuggestionFormComponent } from './feature/suggestion-form/suggestion-form.component';
 import { VoteBannerComponent } from './ui/vote-banner/vote-banner.component';
@@ -17,6 +18,7 @@ import { VoteBannerComponent } from './ui/vote-banner/vote-banner.component';
     FormsSliderComponent,
     GoalSectionComponent,
     HeroCarouselComponent,
+    NavbarComponent,
     RegistrationFormComponent,
     SuggestionFormComponent,
     VoteBannerComponent
