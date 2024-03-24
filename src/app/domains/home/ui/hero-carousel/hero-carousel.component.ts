0a3db7bb89da2618ce0cd694db6c4ac5e1d843b5
Daniel Ope <opeoluwaoyedeji06@gmail.com>
2024-03-24 13:08:26 +0100

    Import `carousel` into `hero-carousel`

--- src/app/domains/home/ui/hero-carousel/hero-carousel.component.ts
+++ src/app/domains/home/ui/hero-carousel/hero-carousel.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { CarouselComponent } from '../../../shared/feature/carousel/carousel.component';
 
 @Component({
   selector: 'hero-carousel',
   standalone: true,
-  imports: [],
+  imports: [CarouselComponent],
   templateUrl: './hero-carousel.component.html',
   styleUrl: './hero-carousel.component.scss'
 })
 export class HeroCarouselComponent {
-
+  slideUrls: string[] = [
+    'https://firebasestorage.googleapis.com/v0/b/nass-eu.appspot.com/o/images%2Fcoding-laptop.jpg?alt=media&token=7e2b1a7e-2fbc-4989-8401-fe6173217443',
+    'https://firebasestorage.googleapis.com/v0/b/nass-eu.appspot.com/o/images%2Fmetal-chairs.jpg?alt=media&token=5f6ddfe7-466d-44ec-a204-c685c8e9261c',
+    'https://firebasestorage.googleapis.com/v0/b/nass-eu.appspot.com/o/images%2Fmovie-camera.jpg?alt=media&token=93505535-0aca-422b-b2f6-714e8622e49f',
+  ];
 }
