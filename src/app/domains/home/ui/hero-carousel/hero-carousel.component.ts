b8a14efdd0e5bcaca4596ac92605f1fe05b89de0
Daniel Ope <opeoluwaoyedeji06@gmail.com>
2024-03-24 14:54:14 +0100

    Set up `hero-image` service in `hero-carousel`

--- src/app/domains/home/ui/hero-carousel/hero-carousel.component.ts
+++ src/app/domains/home/ui/hero-carousel/hero-carousel.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CarouselComponent } from '../../../shared/feature/carousel/carousel.component';
+import { HeroImageService } from '../../data-access/hero-images/hero-image.service';
 
 @Component({
   selector: 'hero-carousel',
@@ -9,9 +10,13 @@ import { CarouselComponent } from '../../../shared/feature/carousel/carousel.com
   styleUrl: './hero-carousel.component.scss'
 })
 export class HeroCarouselComponent {
-  slideUrls: string[] = [
-    'https://firebasestorage.googleapis.com/v0/b/nass-eu.appspot.com/o/images%2Fcoding-laptop.jpg?alt=media&token=7e2b1a7e-2fbc-4989-8401-fe6173217443',
-    'https://firebasestorage.googleapis.com/v0/b/nass-eu.appspot.com/o/images%2Fmetal-chairs.jpg?alt=media&token=5f6ddfe7-466d-44ec-a204-c685c8e9261c',
-    'https://firebasestorage.googleapis.com/v0/b/nass-eu.appspot.com/o/images%2Fmovie-camera.jpg?alt=media&token=93505535-0aca-422b-b2f6-714e8622e49f',
-  ];
+  private heroImageService: HeroImageService = inject(HeroImageService);
+
+  slideUrls: string[] = [];
+
+  ngOnInit(): void {
+    this.heroImageService.getAllImages().then((images) => {
+      this.slideUrls = images;
+    });
+  }
 }
