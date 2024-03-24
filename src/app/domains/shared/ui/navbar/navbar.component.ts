9f850c7427945af463e57c541136c803b72c77dc
Daniel Ope <opeoluwaoyedeji06@gmail.com>
2024-03-24 18:38:03 +0100

    Set up `navbar` mobile view

--- src/app/domains/shared/ui/navbar/navbar.component.ts
+++ src/app/domains/shared/ui/navbar/navbar.component.ts
@@ -8,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  isMenuOpen: boolean = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
 }
