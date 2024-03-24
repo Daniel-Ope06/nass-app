cf26a35b94ea312d44a5ab6f99c311e25075793e
Daniel Ope <opeoluwaoyedeji06@gmail.com>
2024-03-24 13:52:51 +0100

    Set up `carousel` feature

--- src/app/domains/shared/feature/carousel/carousel.component.ts
+++ src/app/domains/shared/feature/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'carousel',
@@ -8,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrl: './carousel.component.scss'
 })
 export class CarouselComponent {
+  @Input({required: true}) slideUrls!: string[];
 
+  @ViewChild('carouselRef', { static: true }) carousel!: ElementRef;
+
+  isLeftScrollDisabled: boolean = true;
+  isRightScrollDisabled: boolean = false;
+
+  scrollLeft(): void {
+    this.carousel.nativeElement.scrollLeft -= window.innerWidth/2;
+  }
+
+  scrollRight(): void {
+    this.carousel.nativeElement.scrollLeft += window.innerWidth/2;
+  }
+
+  disableLeftScroll(): boolean {
+    return this.carousel.nativeElement.scrollLeft <= 10;
+  }
+
+  disableRightScroll(): boolean {
+    let maxScrollLeft = this.carousel.nativeElement.scrollWidth - this.carousel.nativeElement.clientWidth;
+    let scrollRemaining = maxScrollLeft - this.carousel.nativeElement.scrollLeft;
+    return scrollRemaining <= window.innerWidth/6;
+  }
+
+  onScroll(): void {
+    this.isLeftScrollDisabled = this.disableLeftScroll();
+    this.isRightScrollDisabled = this.disableRightScroll();
+  }
 }
