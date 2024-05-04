3c29d5760fd7a2c665adba74ccfefdf9fa7b2172
Daniel Ope <opeoluwaoyedeji06@gmail.com>
2024-05-04 12:55:12 +0100

    Rename `forms-slider` to `news-slider`

--- src/app/domains/home/home.component.ts
+++ src/app/domains/home/home.component.ts
@@ -3,10 +3,10 @@ import { Component } from '@angular/core';
 // Import home domain components
 import { EventsSliderComponent } from './ui/events-slider/events-slider.component';
 import { FooterComponent } from '../shared/ui/footer/footer.component';
-import { FormsSliderComponent } from './ui/forms-slider/forms-slider.component';
 import { GoalSectionComponent } from './ui/goal-section/goal-section.component';
 import { HeroCarouselComponent } from './ui/hero-carousel/hero-carousel.component';
 import { NavbarComponent } from '../shared/ui/navbar/navbar.component';
+import { NewsSliderComponent } from './ui/news-slider/news-slider.component';
 import { RegistrationFormComponent } from './feature/registration-form/registration-form.component';
 import { SuggestionFormComponent } from './feature/suggestion-form/suggestion-form.component';
 import { VoteBannerComponent } from './ui/vote-banner/vote-banner.component';
@@ -17,10 +17,10 @@ import { VoteBannerComponent } from './ui/vote-banner/vote-banner.component';
   imports: [
     EventsSliderComponent,
     FooterComponent,
-    FormsSliderComponent,
     GoalSectionComponent,
     HeroCarouselComponent,
     NavbarComponent,
+    NewsSliderComponent,
     RegistrationFormComponent,
     SuggestionFormComponent,
     VoteBannerComponent,
---
+++ src/app/domains/home/ui/news-slider/news-slider.component.ts
@@ -0,0 +1,22 @@
+import { Component, inject } from '@angular/core';
+import { Slide } from '../../../shared/data-access/slide.model';
+import { SliderComponent } from '../../../shared/feature/slider/slider.component';
+import { NassNewsService } from '../../data-access/news/nass-news.service';
+
+@Component({
+  selector: 'news-slider',
+  standalone: true,
+  imports: [SliderComponent],
+  templateUrl: './news-slider.component.html',
+})
+export class NewsSliderComponent {
+  private nassNewsService: NassNewsService = inject(NassNewsService);
+
+  forms: Slide[] = [];
+
+  ngOnInit(): void {
+    this.nassNewsService.getAllForms().then((forms) => {
+      this.forms = forms;
+    });
+  }
+}
